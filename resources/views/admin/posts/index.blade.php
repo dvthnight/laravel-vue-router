4cf208f70c7e0c734229653e32290ad7f3540f37
dvthnight <94679147+dvthnight@users.noreply.github.com>
2022-05-03 19:31:14 +0200

    dato stile index e link button

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,11 +2,47 @@
 
 @section('content')
 
-    <ul>
-        @foreach ($posts as $post)
-            <h3>{{$post->title}}</h3>
-            <p>{{$post->slug}}</p>
-        @endforeach
-    </ul>
 
+<table class="table table-hover table-dark">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Titolo</th>
+        <th scope="col">Slug</th>
+        <th scope="col">Data di pubblicazione</th>
+        <th scope="col">Data di creazione</th>
+        <th scope="col"></th>
+
+        </tr>
+    </thead>
+    <tbody>
+        
+        @foreach ($posts as $post)
+        <tr>
+            <td>{{$post->id}}</td>
+            <td>{{$post->title}}</td>
+            <td>{{$post->slug}}</td>
+            <td>{{$post->published_at}}</td>
+            <td>{{$post->created_at}}</td>
+            <td>
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    
+                    <form action="{{route("admin.posts.edit",$post)}}" method="POST">
+                        @csrf
+                        @method("GET")
+                        <button type="submit" class="btn btn-secondary">modifica</button>
+                    </form>
+                    <form action="{{route("admin.posts.destroy",$post)}}" method="POST">
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class="btn btn-secondary">elimina</button>
+                    </form>
+                </div>
+            </td>
+        </tr>
+        @endforeach
+        
+    </tbody>
+    </table>
+    
 @endsection
